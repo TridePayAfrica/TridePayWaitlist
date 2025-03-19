e8a533d2f48e4479285b378e01c018aa9a1dcff9
ObinnaEkwealor <Obinna.ekwealor@payaza.africa>
2025-03-19 18:04:52 +0100

    Initial Commit for TridePay Waitlist

---
+++ src/main/java/com/tride/tridewaitlist/repository/WaitlistRepository.java
@@ -0,0 +1,10 @@
+package com.tride.tridewaitlist.repository;
+
+import com.tride.tridewaitlist.model.Waitlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WaitlistRepository extends JpaRepository<Waitlist, Long> {
+    boolean existsByEmail(String email);
+}
---
+++ src/test/java/com/tride/tridewaitlist/TrideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tride.tridewaitlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
