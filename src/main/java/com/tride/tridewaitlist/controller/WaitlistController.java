0e39ea791ee0e2157f443089a6aec2460a1251a5
TridePayAfrica <info@tridepay.africa>
2025-06-22 18:15:41 +0100

    Helpdesk

--- src/main/java/com/tride/tridewaitlist/controller/WaitlistController.java
+++ src/main/java/com/tride/tridewaitlist/controller/WaitlistController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,13 @@ public class WaitlistController {
     @Autowired
     public WaitlistController(WaitlistService waitlistService) {
         this.waitlistService = waitlistService;
+    }
+
+    @GetMapping("/welcome")
+    public ResponseEntity<Map<String, String>> getWelcomeMessage() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(Map.of("Status", "200",
+                        "Message", "Welcome to the Tride Waitlist API!"));
     }
 
     @PostMapping("/add")
